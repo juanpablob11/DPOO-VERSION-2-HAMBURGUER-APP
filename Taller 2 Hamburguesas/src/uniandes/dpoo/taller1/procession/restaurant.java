c48ef23d12f402e789413a145e4b753c9c19f85a
Juan Pablo Barón <111070498+juanpablob11@users.noreply.github.com>
2023-02-19 21:48:34 -0500

    Add files via upload

---
+++ Taller 2 Hamburguesas/src/uniandes/dpoo/taller1/procession/restaurant.java
@@ -0,0 +1,67 @@
+package uniandes.dpoo.taller1.procession;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import uniandes.dpoo.taller1.models.Combo;
+import uniandes.dpoo.taller1.models.Drink;
+import uniandes.dpoo.taller1.models.Ingredient;
+import uniandes.dpoo.taller1.models.Order;
+import uniandes.dpoo.taller1.models.Product;
+import uniandes.dpoo.taller1.procession.restaurant_loader;
+
+public class restaurant {
+	//Attributes
+	private List<Ingredient> Ingredients;
+	private List<Combo> Combos;
+	private List<Product> Products;
+	private List<Drink> Drinks;
+	private List<Order> Orders = new ArrayList<>();
+	//Constructor
+	public restaurant(List<Ingredient> Ingredients, List<Combo> Combos,
+			List<Product> Products, List<Drink> Drinks) {
+		this.Ingredients = Ingredients;
+		this.Combos = Combos;
+		this.Products = Products;
+		this.Drinks = Drinks;
+	}
+	//Methods
+	// Getters 
+	public List<Ingredient> getIngredients(){
+		return Ingredients;
+	}
+	public List<Product> getbaseMenu(){
+		return Products;
+		}
+	public List<Drink> getDrinks(){
+		return Drinks;
+		}
+	public List<Combo> getCombos(){
+		return Combos;
+	}
+	public List<Order> getOrders(){
+		return Orders;
+	}
+	//Main methods
+	public void StartOrder(String customer_name, String customer_address) {
+		Order new_order = new Order(customer_name, customer_address);
+		Orders.add(new_order);
+	}
+	public void closeNsaveOrder() {
+		
+	}
+	public Order getOrderInCourse() {
+		return Orders.get(Orders.size()-1);
+	}
+	public void addElementToOrder(int idorder, Product product) {
+		Order actualOrder = Orders.get(idorder-1);
+		actualOrder.AddProduct(product);
+	}
+	public Order getOrderById(int idorder) {
+		Order actualOrder = Orders.get(idorder-1);
+		return actualOrder;
+	}
+	
+}
